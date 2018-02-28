a030a32f8d8376180e277970f4570afaa090ad5e
Eric Carles <carles.eric@wanadoo.fr>
2018-02-28 16:23:37 +0000

    MongoClient added

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const parser = require('body-parser');
-const server = express();
+const express       = require('express');
+const parser        = require('body-parser');
+const server        = express();
+const MongoClient   = require('mongodb').MongoClient;
+const ObjectID      = require('mongodb').ObjectID;
 
+
+server.use(parser.json());
+server.use(express.static('client/build'));
+server.use(parser.urlencoded({extended: true}));
 
 
 
